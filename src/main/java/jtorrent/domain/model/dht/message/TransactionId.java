34f3fc9c8556718dc116ccaff033947db6acba69
Ashuh <ashuh223@gmail.com>
2023-09-17 04:16:25 +0800

    Implement DhtMessage base class

---
+++ src/main/java/jtorrent/domain/model/dht/message/TransactionId.java
@@ -0,0 +1,67 @@
+package jtorrent.domain.model.dht.message;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public class TransactionId {
+
+    private final char value;
+
+    public TransactionId(char value) {
+        this.value = value;
+    }
+
+    /**
+     * Generates a random transaction id.
+     */
+    public static TransactionId generateRandom() {
+        char value = (char) (Math.random() * Character.MAX_VALUE);
+        return new TransactionId(value);
+    }
+
+    public static TransactionId fromBytes(byte[] bytes) {
+        if (bytes.length != Character.BYTES) {
+            throw new IllegalArgumentException(
+                    String.format("Transaction id must be %d bytes long, but was %d bytes long",
+                            Character.BYTES, bytes.length));
+        }
+        char value = (char) ((Byte.toUnsignedInt(bytes[0]) << 8) | (Byte.toUnsignedInt(bytes[1])));
+        return new TransactionId(value);
+    }
+
+    public char getValue() {
+        return value;
+    }
+
+    public byte[] getBytes() {
+        byte[] bytes = new byte[2];
+        bytes[0] = (byte) ((value >> 8) & 0xFF);
+        bytes[1] = (byte) (value & 0xFF);
+        return bytes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TransactionId that = (TransactionId) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        byte[] bytes = new byte[2];
+        bytes[0] = (byte) ((value >> 8) & 0xFF);
+        bytes[1] = (byte) (value & 0xFF);
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+}
