bacd0bc9389792bb798ac74962b6164bc61692b9
Ashuh <ashuh223@gmail.com>
2023-10-22 02:27:43 +0800

    Add BootstrapTask & RefreshBucketTask

---
+++ src/main/java/jtorrent/domain/manager/dht/task/RefreshBucketTask.java
@@ -0,0 +1,44 @@
+package jtorrent.domain.manager.dht.task;
+
+
+import static jtorrent.domain.util.ValidationUtil.requireNonNull;
+
+import java.lang.System.Logger;
+import java.util.Collection;
+
+import jtorrent.domain.manager.dht.DhtConstants;
+import jtorrent.domain.manager.dht.lookup.FindNodeLookup;
+import jtorrent.domain.model.dht.node.Node;
+import jtorrent.domain.model.dht.node.NodeId;
+import jtorrent.domain.model.dht.routingtable.Bucket;
+import jtorrent.domain.model.dht.routingtable.RoutingTable;
+
+public class RefreshBucketTask implements Runnable {
+
+    private static final Logger LOGGER = System.getLogger(RefreshBucketTask.class.getName());
+
+    /**
+     * The routing table that the bucket belongs to.
+     */
+    private final RoutingTable routingTable;
+    /**
+     * The bucket to refresh.
+     */
+    private final Bucket bucket;
+    private final FindNodeLookup findNodeLookup;
+
+    public RefreshBucketTask(RoutingTable routingTable, Bucket bucket, FindNodeLookup findNodeLookup) {
+        this.routingTable = requireNonNull(routingTable);
+        this.bucket = requireNonNull(bucket);
+        this.findNodeLookup = requireNonNull(findNodeLookup);
+    }
+
+    @Override
+    public void run() {
+        LOGGER.log(Logger.Level.DEBUG, "[DHT] Refreshing bucket {0}", bucket);
+        NodeId randomNodeId = NodeId.randomWithPrefix(bucket.getPrefix());
+        Collection<Node> closestNodes = routingTable.getClosestNodes(randomNodeId, DhtConstants.ALPHA);
+        findNodeLookup.lookup(randomNodeId, closestNodes).forEach(routingTable::addNode);
+        LOGGER.log(Logger.Level.DEBUG, "[DHT] Refreshed bucket {0}", bucket);
+    }
+}
