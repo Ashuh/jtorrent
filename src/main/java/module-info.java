0b87b71dc52bfd5dd4429ae263c056df6103211a
Ashuh <ashuh223@gmail.com>
2023-11-11 05:47:27 +0800

    Fix module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,14 +6,13 @@ module jtorrent {
     requires io.reactivex.rxjava3;
     requires com.dampcake.bencode;
 
-    opens jtorrent.presentation to javafx.graphics;
-    opens jtorrent.presentation.view to javafx.fxml;
     opens jtorrent.common.presentation to javafx.fxml;
     opens jtorrent.peer.presentation to javafx.fxml;
     opens jtorrent.torrent.presentation to javafx.fxml;
-    opens jtorrent.application to javafx.graphics;
     opens jtorrent.application.domain to javafx.graphics;
     opens jtorrent.application.presentation to javafx.fxml, javafx.graphics;
     opens jtorrent.application.presentation.view to javafx.fxml, javafx.graphics;
     opens jtorrent.application.presentation.viewmodel to javafx.fxml, javafx.graphics;
+    opens jtorrent.torrent.presentation.view to javafx.fxml;
+    opens jtorrent.peer.presentation.view to javafx.fxml;
 }
