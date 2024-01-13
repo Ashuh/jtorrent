e876642665bed603b0598ae3d6495e43e48182b6
Ashuh <ashuh223@gmail.com>
2024-01-13 12:15:30 +0800

    Fix DataStatusBar blurry image when stretched

--- src/main/java/jtorrent/presentation/view/DataStatusBar.java
+++ src/main/java/jtorrent/presentation/view/DataStatusBar.java
@@ -18,21 +18,71 @@ public class DataStatusBar extends Pane {
     private final ObjectProperty<Color> availableColor = new SimpleObjectProperty<>(Color.GREEN);
     private final ObjectProperty<Color> unavailableColor = new SimpleObjectProperty<>(Color.RED);
     private final ImageView imageView = new ImageView();
-    private WritableImage writableImage = new WritableImage(1, 1);
+    private WritableImage writableImage;
 
     public DataStatusBar() {
         setPrefHeight(24);
-        imageView.fitWidthProperty().bind(widthProperty());
+        widthProperty().addListener((observable, oldValue, newValue) -> redraw());
+        totalSegments.addListener((observable, oldValue, newValue) -> redraw());
+        availability.addListener((observable, oldValue, newValue) -> updateAvailability(oldValue, newValue));
         imageView.fitHeightProperty().bind(heightProperty());
         getChildren().add(imageView);
-        totalSegments.addListener((observable, oldValue, newValue) -> updateNumSegments(newValue.intValue()));
-        availability.addListener((observable, oldValue, newValue) -> updateAvailability(oldValue, newValue));
     }
 
-    private void updateNumSegments(int numSegments) {
-        WritableImage newImage = new WritableImage(numSegments, 1);
-        for (int i = 0; i < numSegments; i++) {
-            Color color = availability.get().get(i) ? Color.GREEN : Color.RED;
+    /**
+     * Calculates the range of X that belong to the given Y, where X and Y are either pixels or segments.
+     *
+     * @param index        the index of the Y
+     * @param itemsPerUnit the number of X per Y
+     * @return a BitSet containing the range of X that belong to the given Y
+     */
+    private static BitSet calculateRange(int index, double itemsPerUnit) {
+        BitSet range = new BitSet();
+        int firstItem = (int) (index * itemsPerUnit);
+        double nextIndexFirstItem = (index + 1) * itemsPerUnit;
+        boolean isLastItemAligned = isWholeNumber(nextIndexFirstItem);
+        int lastItem = isLastItemAligned ? (int) nextIndexFirstItem - 1 : (int) Math.floor(nextIndexFirstItem);
+        range.set(firstItem, lastItem + 1);
+        return range;
+    }
+
+    private static boolean isWholeNumber(double number) {
+        return number % 1 == 0;
+    }
+
+    private static BitSet computeChangedBits(BitSet previous, BitSet current) {
+        BitSet changed = (BitSet) previous.clone();
+        changed.xor(current);
+        return changed;
+    }
+
+    private Color getColor(double availabilityRatio) {
+        return unavailableColor.get().interpolate(availableColor.get(), availabilityRatio);
+    }
+
+    private BitSet pixelToSegments(int pixel) {
+        return calculateRange(pixel, totalSegments.get() / getWidth());
+    }
+
+    private BitSet segmentToPixels(int segment) {
+        return calculateRange(segment, getWidth() / totalSegments.get());
+    }
+
+    private void redraw() {
+        if (totalSegments.get() == 0) {
+            return;
+        }
+
+        int width = (int) getWidth();
+
+        if (width == 0) {
+            return;
+        }
+
+        WritableImage newImage = new WritableImage(width, 1);
+        for (int i = 0; i < width; i++) {
+            double availableRatio = calculatePixelAvailabilityRatio(i);
+            Color color = getColor(availableRatio);
             newImage.getPixelWriter().setColor(i, 0, color);
         }
 
@@ -43,17 +93,30 @@ public class DataStatusBar extends Pane {
     private void updateAvailability(BitSet previous, BitSet current) {
         BitSet changed = computeChangedBits(previous, current);
         changed.stream()
+                .mapToObj(this::segmentToPixels)
+                .flatMapToInt(BitSet::stream)
+                .distinct()
                 .filter(i -> i < writableImage.getWidth())
                 .forEach(i -> {
-                    Color color = availability.get().get(i) ? Color.GREEN : Color.RED;
+                    double availableRatio = calculatePixelAvailabilityRatio(i);
+                    Color color = getColor(availableRatio);
                     writableImage.getPixelWriter().setColor(i, 0, color);
                 });
     }
 
-    private static BitSet computeChangedBits(BitSet previous, BitSet current) {
-        BitSet changed = (BitSet) previous.clone();
-        changed.xor(current);
-        return changed;
+    /**
+     * Calculates the percentage of segments within the pixel that are available.
+     *
+     * @param pixel the pixel for which to calculate the availability ratio
+     * @return the availability ratio of the pixel as a double between 0 and 1
+     */
+    private double calculatePixelAvailabilityRatio(int pixel) {
+        BitSet segments = pixelToSegments(pixel);
+        BitSet availableSegmentsInPixel = (BitSet) segments.clone();
+        availableSegmentsInPixel.and(availability.get());
+        int totalSegmentsInPixel = segments.cardinality();
+        int availableSegmentsInPixelCount = availableSegmentsInPixel.cardinality();
+        return (double) availableSegmentsInPixelCount / totalSegmentsInPixel;
     }
 
     public IntegerProperty totalSegmentsProperty() {
